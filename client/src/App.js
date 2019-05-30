450d3a0a715293e6c9a16fcc74617673de4c6348
ventti <ehdgus2357@gmail.com>
2019-05-30 22:41:34 +0900

    Add Customer Delete Module

--- client/src/App.js
+++ client/src/App.js
@@ -1,67 +1,66 @@
-import React, { Component } from 'react';
-import './App.css';
-import Customer from './components/Customer';
-import CustomerAdd from './components/CustomerAdd';
-import Paper from '@material-ui/core/Paper';
-import Table from '@material-ui/core/Table';
-import TableHead from '@material-ui/core/TableHead';
-import TableBody from '@material-ui/core/TableBody';
-import TableRow from '@material-ui/core/TableRow';
-import TableCell from '@material-ui/core/TableCell';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import { withStyles } from '@material-ui/core/styles';
+import React, { Component } from "react";
+import "./App.css";
+import Customer from "./components/Customer";
+import CustomerAdd from "./components/CustomerAdd";
+import Paper from "@material-ui/core/Paper";
+import Table from "@material-ui/core/Table";
+import TableHead from "@material-ui/core/TableHead";
+import TableBody from "@material-ui/core/TableBody";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import { withStyles } from "@material-ui/core/styles";
 
 const styles = theme => ({
   root: {
-    width: '100%',
+    width: "100%",
     marginTop: theme.spacing(3),
-    overflowX: 'auto',
+    overflowX: "auto"
   },
   table: {
-    minWidth: 1080,
+    minWidth: 1080
   },
   progress: {
     margin: theme.spacing.unit * 2
   }
-})
+});
 
 class App extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      customers: '',
+      customers: "",
       completed: 0
-    }
+    };
   }
 
   stateRefresh = () => {
     this.setState({
-      customers: '',
+      customers: "",
       completed: 0
     });
     this.callApi()
-      .then(res => this.setState({customers: res}))
+      .then(res => this.setState({ customers: res }))
       .catch(err => console.log(err));
-  }
+  };
 
   componentDidMount() {
     this.timer = setInterval(this.progress, 20);
     this.callApi()
-      .then(res => this.setState({customers: res}))
+      .then(res => this.setState({ customers: res }))
       .catch(err => console.log(err));
   }
 
   callApi = async () => {
-    const response = await fetch('/api/customers');
+    const response = await fetch("/api/customers");
     const body = await response.json();
     return body;
-  }
+  };
 
   progress = () => {
     const { completed } = this.state;
-    this.setState({ completed: completed >= 100 ? 0 : completed + 1});
-  }
+    this.setState({ completed: completed >= 100 ? 0 : completed + 1 });
+  };
 
   render() {
     const { classes } = this.props;
@@ -77,24 +76,43 @@ class App extends Component {
                 <TableCell>생년월일</TableCell>
                 <TableCell>성별</TableCell>
                 <TableCell>직업</TableCell>
+                <TableCell>설정</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
-              {this.state.customers ? this.state.customers.map(c => { return (<Customer key={c.id} id={c.id} image={c.image} 
-              name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>);}) : 
-              <TableRow>
-                <TableCell colSpan="6" align="center">
-                  <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed} />
-                </TableCell>
-              </TableRow>}
+              {this.state.customers ? (
+                this.state.customers.map(c => {
+                  return (
+                    <Customer
+                      stateRefresh={this.stateRefresh}
+                      key={c.id}
+                      id={c.id}
+                      image={c.image}
+                      name={c.name}
+                      birthday={c.birthday}
+                      gender={c.gender}
+                      job={c.job}
+                    />
+                  );
+                })
+              ) : (
+                <TableRow>
+                  <TableCell colSpan="6" align="center">
+                    <CircularProgress
+                      className={classes.progress}
+                      variant="determinate"
+                      value={this.state.completed}
+                    />
+                  </TableCell>
+                </TableRow>
+              )}
             </TableBody>
           </Table>
         </Paper>
-        <CustomerAdd stateRefresh={this.stateRefresh}/>
+        <CustomerAdd stateRefresh={this.stateRefresh} />
       </div>
     );
   }
-  
 }
 
 export default withStyles(styles)(App);
